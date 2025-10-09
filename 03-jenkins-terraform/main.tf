b1a19db05d4ca49e96a0a288058d3fc362f6e432
venkataramireddy1999 <venkataramireddy1999@gmail.com>
2025-10-09 14:13:24 +0530

    Update main.tf

--- 03-jenkins-terraform/main.tf
+++ 03-jenkins-terraform/main.tf
@@ -1,11 +1,12 @@
 provider "aws" {
-  region = "ap-south-1"
+  region = var.aws_region
 }
 
 resource "aws_instance" "my_ec2" {
-  ami           = "ami-0c2b8ca1dad447f8a"  # Amazon Linux 2
-  instance_type = "t2.micro"
+  ami           = var.ami_id
+  instance_type = var.instance_type
+
   tags = {
-    Name = "Jenkins-Terraform-Instance"
+    Name = var.instance_name
   }
 }
